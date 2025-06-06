9ae2e1849b1c6032bf769b5c145561232b0f1136
Kanson Chu <kansonchuhc@gmail.com>
2025-06-06 22:43:55 +0800

    Add app.py to code folder

---
+++ code/app.py
@@ -0,0 +1,49 @@
+from random import choice
+
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+window_titles = [
+    "My App",
+    "My App",
+    "Still My App",
+    "Still My App",
+    "What on earth",
+    "What on earth",
+    "This is surprising",
+    "This is surprising",
+    "Something went wrong",
+]
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.n_times_clicked = 0
+
+        self.setWindowTitle("My App")
+
+        self.button = QPushButton("Press Me!")
+        self.button.clicked.connect(self.the_button_was_clicked)
+
+        self.windowTitleChanged.connect(self.the_window_title_changed)
+
+        self.setCentralWidget(self.button)
+
+    def the_button_was_clicked(self):
+        print("Clicked.")
+        new_window_title = choice(window_titles)
+        print(f"Setting titles: {window_titles}")
+        self.setWindowTitle(new_window_title)
+
+    def the_window_title_changed(self, window_title):
+        print(f"Window title changed: {window_title}")
+
+        if window_title == "Something went wrong":
+            self.button.setDisabled(True)
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()
