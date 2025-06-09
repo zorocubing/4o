ee540b0d53af1b130eeb326e0fad7627e7853ad1
Kanson Chu <kansonchuhc@gmail.com>
2025-06-09 14:58:47 +0800

    Update code/app.py with latest PySide6 mouse event example

--- code/app.py
+++ code/app.py
@@ -1,47 +1,34 @@
-from random import choice
+import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
-
-window_titles = [
-    "My App",
-    "My App",
-    "Still My App",
-    "Still My App",
-    "What on earth",
-    "What on earth",
-    "This is surprising",
-    "This is surprising",
-    "Something went wrong",
-]
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QLabel
+)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.n_times_clicked = 0
+        self.label = QLabel("Click in this window!")
+        self.setCentralWidget(self.label)
+        self.setMouseTracking(True)
+        self.label.setMouseTracking(True)
 
-        self.setWindowTitle("My App")
+    def mouseMoveEvent(self, e):
+        self.label.setText("mouseMoveEvent")
 
-        self.button = QPushButton("Press Me!")
-        self.button.clicked.connect(self.the_button_was_clicked)
+    def mousePressEvent(self, e):
+        self.label.setText("mousePressEvent")
 
-        self.windowTitleChanged.connect(self.the_window_title_changed)
+    def mouseReleaseEvent(self, e):
+        self.label.setText("mouseReleaseEvent")
 
-        self.setCentralWidget(self.button)
+    def mouseDoubleClickEvent(self, e):
+        self.label.setText("mouseDoubleClickEvent")
 
-    def the_button_was_clicked(self):
-        print("Clicked.")
-        new_window_title = choice(window_titles)
-        print(f"Setting titles: {window_titles}")
-        self.setWindowTitle(new_window_title)
 
-    def the_window_title_changed(self, window_title):
-        print(f"Window title changed: {window_title}")
-
-        if window_title == "Something went wrong":
-            self.button.setDisabled(True)
-
-app = QApplication([])
+app = QApplication(sys.argv)
 
 window = MainWindow()
 window.show()
