0b1851261aeb9787aa01a8e47be1d9cf5a6b89be
Kanson Chu <kansonchuhc@gmail.com>
2025-06-11 21:43:41 +0800

    Initial Commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QPushButton
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("4o")
+        self.resize(400, 600)
+
+        button = QPushButton("Open File")
+        self.setCentralWidget(button)
+        button.clicked.connect(self.open_folder)
+
+    def open_folder(self):
+        folder = QFileDialog.getExistingDirectory(self, "Select Folder", "")
+        print(f"Selected folder: {folder}")
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()
