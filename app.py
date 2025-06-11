9398a027e6ab828afe91cb69062480665f5ce6c8
Kanson Chu <kansonchuhc@gmail.com>
2025-06-11 22:37:38 +0800

    how long are you gonna take to commit

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QPushButton
+import os
+from PySide6.QtWidgets import QWidget, QApplication, QMainWindow, QFileDialog, QPushButton
 
+class ConfirmWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Child Window")
+        self.resize(200, 100)
+        self.setStyleSheet("background-color: lightblue;")
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("4o")
-        self.resize(400, 600)
+        self.resize(200, 200)
 
-        button = QPushButton("Open File")
+        button = QPushButton("Open Folder")
         self.setCentralWidget(button)
         button.clicked.connect(self.open_folder)
 
     def open_folder(self):
         folder = QFileDialog.getExistingDirectory(self, "Select Folder", "")
         print(f"Selected folder: {folder}")
+
 
 app = QApplication([])
 
