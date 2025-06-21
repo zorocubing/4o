9b0a4adf8620d93de08be974937d59a514ae9b2c
Kanson Chu <kansonchuhc@gmail.com>
2025-06-21 11:26:09 +0800

    Updated

--- app.py
+++ app.py
@@ -27,13 +27,14 @@ class ConfirmDialog(QDialog):
         # Define file extensions
         image_extension = (".jpg", ".jpeg", ".png", ".gif")
         video_extension = (".mp4", ".mov", ".webm", ".mkv")
-        document_extension = (".doc", ".docx", ".odt", ".pdf")
+        document_extension = (".doc", ".docx", ".odt", ".pdf", ".pptx", ".gslides")
+        audio_extension = (".mp3", ".wav", ".aiff", ".flac", ".aac")
         code_extension = (".py", ".js", ".html", ".css")
 
         global selected_folder
         ddir = selected_folder
 
-        subdirectories = ["Images", "Videos", "Documents", "Code", "Others"]
+        subdirectories = ["Images", "Videos", "Documents", "Code", "Audio", "Others"]
         for subdirectory in subdirectories:
             path = os.path.join(ddir, subdirectory)
             if not os.path.exists(path):
@@ -43,6 +44,7 @@ class ConfirmDialog(QDialog):
         def get_videos(file): return os.path.splitext(file)[1].lower() in video_extension
         def get_document(file): return os.path.splitext(file)[1].lower() in document_extension
         def get_code(file): return os.path.splitext(file)[1].lower() in code_extension
+        def get_audio(file): return os.path.splitext(file)[1].lower() in audio_extension
 
         for file in os.listdir(ddir):
             file_path = os.path.join(ddir, file)
@@ -57,6 +59,8 @@ class ConfirmDialog(QDialog):
                     shutil.move(file_path, os.path.join(ddir, "Documents", file))
                 elif get_code(file):
                     shutil.move(file_path, os.path.join(ddir, "Code", file))
+                elif get_audio(file):
+                    shutil.move(file_path, os.path.join(ddir, "Audio", file))
                 else:
                     shutil.move(file_path, os.path.join(ddir, "Others", file))
             except Exception as e:
