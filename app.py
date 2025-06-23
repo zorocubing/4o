562280e09d024f84b37e9df88d5d89274b78360e
Kanson Chu <kansonchuhc@gmail.com>
2025-06-23 22:38:00 +0800

    v1.1 Update

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ class ConfirmDialog(QDialog):
         # Define file extensions
         image_extension = (".jpg", ".jpeg", ".png", ".gif")
         video_extension = (".mp4", ".mov", ".webm", ".mkv")
-        document_extension = (".doc", ".docx", ".odt", ".pdf", ".txt", "pptx")
+        document_extension = (".doc", ".docx", ".odt", ".pdf", ".txt", ".pptx", ".xlsx", ".csv")
         code_extension = (".py", ".js", ".html", ".css")
         audio_extension = (".mp3", ".wav", ".aiff", ".flac", ".aac")
 
